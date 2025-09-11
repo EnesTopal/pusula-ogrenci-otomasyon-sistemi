752149d837a2c975aedbc3977d80c78fc3f43909
EnesTopal <enes.topal1@gazi.edu.tr>
2025-09-11 13:14:09 +0300

    absence and course adding

--- Pusula.UI/obj/Debug/net9.0/Pusula.UI.AssemblyInfo.cs
+++ Pusula.UI/obj/Debug/net9.0/Pusula.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pusula.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+311c64049eaaf1970b549dfc07d0accf7458d69c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76a34be67b69cacc1d1d85fd2eefee1290f43e49")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pusula.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pusula.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
